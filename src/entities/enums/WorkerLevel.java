16f2126e98b827acc2319b6e3c9e8bff7d79b8a0
wellington Machado <dev.wellmac@gmail.com>
2022-03-06 20:02:36 -0300

    Project created

---
+++ src/entities/enums/WorkerLevel.java
@@ -0,0 +1,19 @@
+package entities.enums;
+
+public class WorkerLevel {
+
+	
+	JUNIOR, 
+	MID_LEVEL,
+	SENIOR;
+	
+
+	public static WorkerLevel valueOf(String workerLevel) {
+	
+		
+	
+	}
+	
+    
+
+}
